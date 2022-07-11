5d0f5cac577d4aa7f55fd56dc64e93b600ac9362
andresnow <andre_neve@hotmail.com>
2022-07-11 18:27:47 -0300

    criacao das rotas

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -14,6 +14,7 @@ class StoreController extends Controller
     public function index()
     {
         //
+        exit('index');
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('store')->group(function () {
+    Route::get('/', [StoreController::class, 'index']);
+    Route::get('/{id}', [StoreController::class, 'show']);
+    Route::post('/', [StoreController::class, 'store']);
+    Route::put('/{id}', [StoreController::class, 'update']);
+    Route::delete('/{id}', [StoreController::class, 'destroy']);
+});
+
+Route::prefix('products')->group(function () {
+    Route::get('', [ProductController::class, 'index']);
+    Route::get('{id}', [ProductController::class, 'show']);
+    Route::post('', [ProductController::class, 'store']);
+    Route::put('{id}', [ProductController::class, 'update']);
+    Route::patch('{id}', [ProductController::class, 'upgrade']);
+    Route::delete('{id}', [ProductController::class, 'destroy']);
 });
