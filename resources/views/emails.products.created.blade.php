56d4fd842932d5ba29814ad2cd432113008da2ad
andresnow <andre_neve@hotmail.com>
2022-07-13 12:57:05 -0300

    primeiros ajustes do envio de email

---
+++ resources/views/emails.products.created.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- the function env get the environment variables from .env file --}}
+    <title>{{ env('APP_NAME') }}</title>
+</head>
+
+<body>
+    <h1>Novo Produto</h1>
+    <p>
+        Um novo produto foi criado no sistema.
+</body>
+</html>
